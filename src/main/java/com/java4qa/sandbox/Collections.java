2669b489b43abf5e200627dfd0d406e848c52bc6
Alexey Isakov <isakov.ktk@gmail.com>
2017-12-14 12:34:13 +0200

    Update files.

---
+++ src/main/java/com/java4qa/sandbox/Collections.java
@@ -0,0 +1,33 @@
+package com.java4qa.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+//        List<String> languages = new ArrayList<String>();
+//        languages.add("Java");
+//        languages.add("C#");
+//        languages.add("Python");
+//        languages.add("PHP");
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+        for (String l : languages){
+            System.out.println("I want to learn " + l);
+        }
+
+//        for (int i =0; i < languages.size(); i++){
+//            System.out.println("I want to learn " + languages.get(i));
+//        }
+
+//        List languages = Arrays.asList("Java", "C#", "Python", "PHP");
+//        for (Object l : languages) {
+//            System.out.println("I want to learn " + l);
+//        }
+    }
+}
+
